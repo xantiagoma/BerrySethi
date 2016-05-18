070b08a7152fa86f0273b2ba64cdb7751c95cbc4
Santiago Montoya <sntiagoma@gmail.com>
2016-05-18 16:37:43 -0500

    NumSym

---
+++ Parser/NumSym.hs
@@ -0,0 +1,31 @@
+module NumSym(NumSym(..)) where
+data NumSym a = NEp
+              | NS Int a
+              | NTerm
+
+instance (Eq a) => Eq (NumSym a) where
+  NEp      == (NS x y)   = False
+  NEp      == NEp        = True
+  NEp      == NTerm      = False
+  (NS x y) == NEp        = False
+  (NS x y) == (NS x' y') = x==x' && y==y'
+  (NS x y) == NTerm      = False
+  NTerm    == NEp        = False
+  NTerm    == (NS x y)   = False
+  NTerm    == NTerm      = True
+
+instance (Ord a) => Ord (NumSym a) where
+  compare NEp      NEp        = EQ
+  compare NEp      (NS x y)   = LT
+  compare NEp      NTerm      = LT
+  compare (NS x y) NEp        = GT
+  compare (NS x y) (NS x' y') = compare x x'
+  compare (NS x y) NTerm      = LT
+  compare NTerm    NEp        = GT
+  compare NTerm    (NS x y)   = GT
+  compare NTerm    NTerm      = EQ
+
+instance (Show a) => Show (NumSym a) where
+  show NEp = "€"
+  show (NS num d) = (show d)++"_"++(show num)
+  show NTerm = "˧"
--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -1,41 +1,61 @@
-module Parser where
+module Parser(parseRegExpr) where
 
 import RegExpr
+import NumSym
 import Text.ParserCombinators.Parsec
 
 
 
-pA :: GenParser Char st String
+pA :: GenParser Char st Char
 pA = do l <- alphaNum
-        ls <- many alphaNum
-        return $ (l:ls)
+        return $ (l)
 
-parseRegExpr :: SourceName -> String -> Either ParseError (RegExpr String)
+parseRegExpr :: SourceName -> String -> Either ParseError (RegExpr Char)
+parseRegExpr source "" = parse pEmpty source " "
 parseRegExpr source st = parse pRegExpr source st    
 
-pSym :: GenParser Char st (RegExpr String)
+pSym :: GenParser Char st (RegExpr Char)
 pSym = Sym <$> pA
 
-pKleene :: GenParser Char st (RegExpr String)
+pEmpty :: GenParser Char st (RegExpr a)
+pEmpty = Empty <$ char ' '
+
+pOpKleene :: GenParser Char st (RegExpr Char -> RegExpr Char)
+pOpKleene = Kleene <$ char '*' 
+         <?> "Expected '*'"
+
+pKleene :: GenParser Char st (RegExpr Char)
 pKleene = try (Kleene <$> (pFactor <* char '*'))
   <|> pFactor
 
-pOpUnion :: GenParser Char st (RegExpr String -> RegExpr String -> RegExpr String)
+pOpUnion :: GenParser Char st (RegExpr Char -> RegExpr Char -> RegExpr Char)
 pOpUnion = Union <$ char '|'
          <?> "Expected '|'"
 
-pConcat :: GenParser Char st (RegExpr String)
+pConcat :: GenParser Char st (RegExpr Char)
 pConcat = pKleene `chainl1` pOpConcat
 
-pOpConcat :: GenParser Char st (RegExpr String -> RegExpr String -> RegExpr String)
+pOpConcat :: GenParser Char st (RegExpr Char -> RegExpr Char -> RegExpr Char)
 pOpConcat = Con <$ char '.'
         <?> "Expected '.'"
 
-pRegExpr :: GenParser Char st (RegExpr String)
+pRegExpr :: GenParser Char st (RegExpr Char)
 pRegExpr = pConcat `chainl1` pOpUnion
 
-pFactor :: GenParser Char st (RegExpr String)
+pFactor :: GenParser Char st (RegExpr Char)
 pFactor = Sym <$> pA
        <|> pParens pRegExpr
 
 pParens = between(char '(') (char ')')
+
+toRegExprNum :: (Int,RegExpr a) -> (Int, RegExpr (NumSym a))
+toRegExprNum (i,Empty)      = (i, Sym (NEp) )
+toRegExprNum (i,Sym a)      = (i+1, Sym (NS i a) )
+toRegExprNum (i, Con l r)   = let (i',l') = toRegExprNum (i,l)
+                                  (i'',r') = toRegExprNum (i',r)
+                                  in (i'', Con l' r')
+toRegExprNum (i, Union l r) = let (i',l') = toRegExprNum (i,l)
+                                  (i'',r') = toRegExprNum (i',r)
+                                  in (i'', Union l' r')
+toRegExprNum (i, Kleene h)  = let (i', h') = toRegExprNum (i,h)
+                                  in (i', Kleene h')
